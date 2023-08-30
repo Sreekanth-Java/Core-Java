2c66f81d5c63fdd9b33c3f0c85ad021536080292
Sreekanth-Java <143527918+Sreekanth-Java@users.noreply.github.com>
2023-08-30 08:38:32 +0530

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,12 @@
+package com.stringsEx;
+
+public class StringReverse {
+	
+	public static void main(String[] args) {
+		String st="Rahul";
+		StringBuffer sb=new StringBuffer(st);
+		sb.reverse();
+		System.out.println("String reverse: "+sb);
+	}
+
+}
