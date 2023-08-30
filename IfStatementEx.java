a0451776de44d93fc8f522972c602f32f6fda599
Sreekanth-Java <143527918+Sreekanth-Java@users.noreply.github.com>
2023-08-30 08:39:16 +0530

    Add files via upload

---
+++ IfStatementEx.java
@@ -0,0 +1,16 @@
+package com.controlStatements;
+
+public class IfStatementEx {
+	
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int z=x+y;
+		
+		if(z<20) {
+			System.out.println("Z is greater than 20");
+		}else {
+			System.out.println("Z is less than 20");
+		}
+	}
+}
